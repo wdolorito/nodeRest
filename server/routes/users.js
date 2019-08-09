6966c14e3c5c6706159ef2a48d0e8c0b7fd717fb
William Dolorito <wdolorito@gmail.com>
2019-08-09 01:31:58 -0700

    Get users, get one user, create user endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,49 @@
 const errors = require('restify-errors')
+const User = require('../models/User')
 
 module.exports = server => {
   //CRUD operations => post get put del
   server.post('/user', async (req, res, next) => {
+    if(!req.is('application/json')) {
+      return next(new errors.InvalidContentError('Data not sent correctly'))
+    }
+
+    const { userName,
+            firstName,
+            middleName,
+            lastName,
+            email,
+            password,
+            location,
+            bio,
+            avatar } = req.body
+
+    const user = new User({
+      userName,
+      firstName,
+      middleName,
+      lastName,
+      email,
+      password,
+      location,
+      bio,
+      avatar
+    })
+
     try {
-      res.send(new errors.BadRequestError('create a user')) // 201 req
+      const newUser = await user.save()
+      res.send(201)
       next()
     } catch(err) {
-      return next(new errors.BadRequestError(err))
+      console.log(err)
+      return next(new errors.InternalError(err.message))
     }
   })
 
   server.get('/users', async (req, res, next) => {
     try {
-      res.send(new errors.InvalidContentError('get all users'))
+      const users = await User.find()
+      res.send(users)
       next()
     } catch(err) {
       return next(new errors.InvalidContentError(err))
@@ -22,28 +52,29 @@ module.exports = server => {
 
   server.get('/users/:id', async (req, res, next) => {
     try {
-      res.send(new errors.InvalidContentError('get one user'))
+      const user = await User.findById(req.params.id)
+      res.send(user)
       next()
     } catch(err) {
-      return next(new errors.InvalidContentError(err))
+      return next(new errors.ResourceNotFoundError('No user with id: ' + req.params.id))
     }
   })
 
-  server.put('/user/:id', async (req, res, next) => { // 200 req
-    try {
-      res.send(new errors.InvalidArgumentError('update a user'))
-      next()
-    } catch(err) {
-      return next(new errors.InvalidArgumentError(err)) // || MissingParameterError()
-    }
-  })
-
-  server.del('/user/:id', async (req, res, next) => { // 204 req
-    try {
-      res.send(new errors.GoneError('delete a user'))
-      next()
-    } catch(err) {
-      return next(new errors.GoneError(err))
-    }
-  })
+  // server.put('/user/:id', async (req, res, next) => { // 200 req
+  //   try {
+  //     res.send(new errors.InvalidArgumentError('update a user'))
+  //     next()
+  //   } catch(err) {
+  //     return next(new errors.InvalidArgumentError(err)) // || MissingParameterError()
+  //   }
+  // })
+  //
+  // server.del('/user/:id', async (req, res, next) => { // 204 req
+  //   try {
+  //     res.send(new errors.GoneError('delete a user'))
+  //     next()
+  //   } catch(err) {
+  //     return next(new errors.GoneError(err))
+  //   }
+  // })
 }
